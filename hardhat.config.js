b5efe4f470eeaf1c19949dbf27f38bc2a6077aa3
Rishikesh Kale <rk@Rishikeshs-MacBook-Air.local>
2022-10-26 09:46:01 +0530

    Updated config

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,7 +10,7 @@ module.exports = {
   networks: {
     wallaby: {
       url: "https://wallaby.node.glif.io/rpc/v0",
-      accounts: ["da4d7cb20fe0f9ebe07de9d218a45ac6cb7f8d7d47ba2b108c4b690e817fb8f4"],
+      accounts: [PRIVATE_KEY],
     }
   },
   paths: {
