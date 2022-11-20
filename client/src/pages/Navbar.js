85c7c08b836e31410b5921a5bf999f9075bc4f63
Anubha13kumari <anubhamuz@gmail.com>
2022-11-20 15:32:37 +0530

    added logo and images

--- client/src/pages/Navbar.js
+++ client/src/pages/Navbar.js
@@ -3,6 +3,7 @@ import { ethers } from "ethers";
 import { CONTRACT_ADDRESS } from "../Constants";
 import { CONTRACT_ABI } from "../Constants";
 import "./Navbar.css";
+import Logo from "../assets/Logo.png"
 
 function Navbar({ isMainApp }) {
   const onButtonClick = () => {
@@ -17,34 +18,37 @@ function Navbar({ isMainApp }) {
 
   const [publicKey, setPublickey] = useState();
 
-    const [chainId, setChainId] = useState();
-    const [msg, setMsg] = useState();
-    const [notice, setNotice] = useState();
-    const [noticedBy, setNoticeBy] = useState();
-    const [updatedNotice, setUpdatedNotice] = useState();
+  const [chainId, setChainId] = useState();
+  const [msg, setMsg] = useState();
+  const [notice, setNotice] = useState();
+  const [noticedBy, setNoticeBy] = useState();
+  const [updatedNotice, setUpdatedNotice] = useState();
 
-    const connectButton = async () => {
-      const { ethereum } = window;
-      if (ethereum.isMetaMask) {
-        const provider = new ethers.providers.Web3Provider(ethereum, "any");
-        const accounts = await provider.send("eth_requestAccounts", []);
-        const { chainId } = await provider.getNetwork();
-        setChainId(chainId);
-        setPublickey(accounts[0]);
-        console.log("accounts: ",accounts[0]);
-        // await readNotice(provider);
-      } else {
-        setMsg("Install MetaMask");
-      }
-    };
+  const connectButton = async () => {
+    const { ethereum } = window;
+    if (ethereum.isMetaMask) {
+      const provider = new ethers.providers.Web3Provider(ethereum, "any");
+      const accounts = await provider.send("eth_requestAccounts", []);
+      const { chainId } = await provider.getNetwork();
+      setChainId(chainId);
+      setPublickey(accounts[0]);
+      console.log("accounts: ", accounts[0]);
+      // await readNotice(provider);
+    } else {
+      setMsg("Install MetaMask");
+    }
+  };
 
   return (
     <div className="nav__container">
       <div>
+        <div className="nav__logocontainer">
+          <img src={Logo} alt="logo"></img>
+        </div>
         <div className="nav__buttoncontainer">
           {isMainApp ? (
             <button className="nav__button" onClick={connectButton}>
-                {publicKey !== undefined && publicKey!== null?`${publicKey?.slice(0,7)}...${publicKey?.slice(35)}`:"Connect Wallet"}
+              {publicKey !== undefined && publicKey !== null ? `${publicKey?.slice(0, 7)}...${publicKey?.slice(35)}` : "Connect Wallet"}
             </button>
           ) : (
             <button className="nav__button" onClick={onButtonClick}>
