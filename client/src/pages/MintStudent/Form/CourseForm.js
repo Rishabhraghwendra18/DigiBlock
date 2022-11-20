24e6a584b6db9eacfbf4579b8dee79fd61778153
rishabhraghwendra@18 <rishabhraghwendra2002@gmail.com>
2022-11-20 12:43:36 +0530

    added snacks and nft successfuly uploading to nft.storage

--- client/src/pages/MintStudent/Form/CourseForm.js
+++ client/src/pages/MintStudent/Form/CourseForm.js
@@ -1,43 +1,98 @@
 import {
-    Button,
-    Card,
-    CardContent,
-    FormLabel,
-    Grid,
-    TextField,
-    Typography,
-  } from "@mui/material";
-  // import IconButton from "@mui/material";
-  import AddIcon from "@mui/icons-material/Add";
-  import RemoveIcon from "@mui/icons-material/Remove";
-  import React, { useState } from "react";
-  
-  function Form({ title }) {
-    const [coursesList, setCoursesList] = useState([
-      { courseName: "", grade: null },
-    ]);
-  
-    const handleChangeInput = (index, event) => {
-      const values = [...coursesList];
-      values[index][event.target.name] = event.target.value;
-      setCoursesList(values);
-    };
-    const addCourses = () => {
-      setCoursesList([...coursesList, { courseName: "", grade: null }]);
-    };
-    const removeCourse = (index) => {
-      const values = [...coursesList];
-      values.splice(index, 1);
-      setCoursesList(values);
-    };
-    return (
-      <div>
-        <Typography gutterBottom variant="h5" align="center">
-          {title}
-        </Typography>
-        <Card sx={{padding:'20px 5px'}}>
-          <CardContent>
-              <form>
+  Button,
+  Card,
+  CardContent,
+  FormLabel,
+  Grid,
+  TextField,
+  Typography,
+  Snackbar,
+  Alert
+} from "@mui/material";
+// import IconButton from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
+import React, { useState } from "react";
+import { NFTStorage, File } from "nft.storage";
+
+function Form({ title }) {
+  const [soulFirstName, setSoulFirstName] = useState();
+  const [soulLastName, setSoulLastName] = useState();
+  const [courseName, setCourseName] = useState();
+  const [remarks, setRemarks] = useState();
+  const [coursesList, setCoursesList] = useState([
+    { courseName: "", grade: null },
+  ]);
+  const [snackBarState, setSnackBarState] = useState({
+    open: false,
+    vertical: "top",
+    horizontal: "right",
+    description:"Metadata is uploading to IPFS & Filecoin"
+  });
+  const { vertical, horizontal, open,description } = snackBarState;
+
+  const handleChangeInput = (index, event) => {
+    const values = [...coursesList];
+    values[index][event.target.name] = event.target.value;
+    setCoursesList(values);
+  };
+  const addCourses = () => {
+    setCoursesList([...coursesList, { courseName: "", grade: null }]);
+  };
+  const removeCourse = (index) => {
+    const values = [...coursesList];
+    values.splice(index, 1);
+    setCoursesList(values);
+  };
+
+  const handleSnackBarOpen = (description) => {
+    setSnackBarState({ ...snackBarState, open: true,description });
+  };
+  const handleSnackBarClose = () => {
+    setSnackBarState({ ...snackBarState, open: false });
+  };
+
+  async function storeAsset() {
+    const fileLogo = await fetch("../../../assets/HarvadLogo.png");
+    const blob = await fileLogo.blob();
+
+    const API_KEY = process.env.REACT_APP_NFT_STORAGE_API;
+    const client = new NFTStorage({ token: API_KEY });
+    const date = new Date();
+
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    let currentDate = `${day}-${month}-${year}`;
+    handleSnackBarOpen('Uploading Metadata to IPFS & Filecoin. Please wait till its uploaded.');
+
+    const metadata = await client.store({
+      name: `${soulFirstName} ${soulLastName} ${courseName} record`,
+      description: `${soulFirstName} ${soulLastName} ${courseName} record. Minted on ${currentDate}`,
+      image: new File([blob], "HarvadLogo.png", { type: "image/png" }),
+      properties: {
+        coursesList,
+        remarks,
+      },
+    });
+    handleSnackBarOpen('Minting Soul Token...');
+    console.clear();
+    console.log("Metadata stored on Filecoin and IPFS with URL:", metadata.url);
+    handleSnackBarOpen('Minted successfully');
+  }
+
+  const onSubmit = (event) => {
+    storeAsset();
+    event.preventDefault();
+  };
+  return (
+    <div>
+      <Typography gutterBottom variant="h5" align="center">
+        {title}
+      </Typography>
+      <Card sx={{ padding: "20px 5px" }}>
+        <CardContent>
+          <form onSubmit={onSubmit}>
             <Grid container spacing={1}>
               <Grid xs={12} item>
                 <TextField
@@ -48,38 +103,41 @@ import {
                   required
                 />
               </Grid>
-  
+
               <Grid xs={12} sm={6} item>
                 <TextField
                   label="Soul First Name"
                   placeholder="Enter Firsts Name"
                   variant="outlined"
+                  onChange={(e) => setSoulFirstName(e.target.value)}
                   fullWidth
                   required
                 />
               </Grid>
-  
+
               <Grid xs={12} sm={6} item>
                 <TextField
                   label="Soul Last Name"
                   placeholder="Enter Last Name"
                   variant="outlined"
+                  onChange={(e) => setSoulLastName(e.target.value)}
                   fullWidth
                   required
                 />
               </Grid>
-  
-              <Grid xs={12} item sx={{marginBottom:'30px'}}>
+
+              <Grid xs={12} item sx={{ marginBottom: "30px" }}>
                 <TextField
                   type={"text"}
                   label="Course Name"
                   placeholder="Enter Course Name"
                   variant="outlined"
+                  onChange={(e) => setCourseName(e.target.value)}
                   fullWidth
                   required
                 />
               </Grid>
-  
+
               <Grid xs={12} sm={10} item>
                 <FormLabel
                   gutterBottom
@@ -109,7 +167,7 @@ import {
                       required
                     />
                   </Grid>
-  
+
                   <Grid xs={12} sm={4} item key={index}>
                     <TextField
                       name="grade"
@@ -128,19 +186,20 @@ import {
                   </Grid>
                 </>
               ))}
-  
-              <Grid xs={12} item sx={{marginBottom:5}}>
+
+              <Grid xs={12} item sx={{ marginBottom: 5 }}>
                 <TextField
                   multiline
                   rows={4}
                   label="Remarks"
                   placeholder="Enter Remarks (Press enter to go to next line)"
                   variant="outlined"
+                  onChange={(e) => setRemarks(e.target.value)}
                   fullWidth
                   required
                 />
               </Grid>
-  
+
               <Grid xs={12} item>
                 <Button
                   type="submit"
@@ -152,12 +211,16 @@ import {
                 </Button>
               </Grid>
             </Grid>
-            </form>
-          </CardContent>
-        </Card>
-      </div>
-    );
-  }
-  
-  export default Form;
-  
+          </form>
+        </CardContent>
+      </Card>
+      <Snackbar open={open} anchorOrigin={{ vertical, horizontal }} autoHideDuration={6000} onClose={handleSnackBarClose}>
+        <Alert onClose={handleSnackBarClose} severity="info" sx={{ width: "100%" }}>
+            {description}
+        </Alert>
+      </Snackbar>
+    </div>
+  );
+}
+
+export default Form;
